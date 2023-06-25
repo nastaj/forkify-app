b52fe7173aa90e70055e1959435a77f8d0ad6c33
nastaj <G00424689@atu.ie>
2023-06-25 18:13:04 +0200

    servings

--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -20,55 +20,40 @@ class PaginationView extends View {
     const numPages = Math.ceil(
       this._data.results.length / this._data.resultsPerPage
     );
-
-    // Page 1, and there are other pages
-    if (curPage === 1 && numPages > 1) {
-      return `
-      <button data-goto="${
-        curPage + 1
-      }" class="btn--inline pagination__btn--next">
-        <span>Page ${curPage + 1}</span>
-        <svg class="search__icon">
-            <use href="${icons}#icon-arrow-right"></use>
-        </svg>
-      </button>
-      `;
-    }
-
-    // Last page
-    if (curPage === numPages && numPages > 1) {
-      return `
-        <button data-goto="${
-          curPage - 1
-        }" class="btn--inline pagination__btn--prev">
-            <svg class="search__icon">
-                <use href="${icons}#icon-arrow-left"></use>
-            </svg>
-            <span>Page ${curPage - 1}</span>
-        </button>
-      `;
-    }
-
-    // Other page
-    if (curPage < numPages) {
-      return `
-      <button data-goto="${
-        curPage - 1
-      }" class="btn--inline pagination__btn--prev">
+    const btnGoForward = `
+    <button data-goto="${
+      curPage + 1
+    }" class="btn--inline pagination__btn--next">
+      <span>Page ${curPage + 1}</span>
+      <svg class="search__icon">
+          <use href="${icons}#icon-arrow-right"></use>
+      </svg>
+    </button>
+    `;
+    const btnGoPrevious = `
+    <button data-goto="${
+      curPage - 1
+    }" class="btn--inline pagination__btn--prev">
         <svg class="search__icon">
             <use href="${icons}#icon-arrow-left"></use>
         </svg>
         <span>Page ${curPage - 1}</span>
-      </button>
-      <button data-goto="${
-        curPage + 1
-      }" class="btn--inline pagination__btn--next">
-        <span>Page ${curPage + 1}</span>
-        <svg class="search__icon">
-            <use href="${icons}#icon-arrow-right"></use>
-        </svg>
-      </button>
-      `;
+    </button>
+    `;
+
+    // Page 1, and there are other pages
+    if (curPage === 1 && numPages > 1) {
+      return btnGoForward;
+    }
+
+    // Last page
+    if (curPage === numPages && numPages > 1) {
+      return btnGoPrevious;
+    }
+
+    // Other page
+    if (curPage < numPages) {
+      return btnGoForward + btnGoPrevious;
     }
 
     // Page 1, and there are NO other pages
